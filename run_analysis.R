cf9d936d4858c3f39ea202678018e8f0d109e038
leifulstrup <leifulstrup@users.noreply.github.com>
2014-05-19 20:46:29 -0400

    First Commit of draft files for the Coursera Project

---
+++ run_analysis.R
@@ -0,0 +1,5 @@
+# Coursera
+# Getting and Cleaning Data
+# Data Science Specialization Course
+# Class Project
+# "The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set."
